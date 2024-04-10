5fd44728e708e12b849bb70fa0f5015e21e59b8f
Bharat <mail2agrawalbharat@gmail.com>
2024-04-10 18:04:16 +0530

    Initial Commit

---
+++ Weather Forecast/Weather_ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ForecastApp.swift
+//  Weather Forecast
+//
+//  Created by Aman Agrawal on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
